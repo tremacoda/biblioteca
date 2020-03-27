08113b09208b29b6520477cd44b773b03c89df1a
tremacoda <mario.bergognoni@gmail.com>
2020-03-27 15:43:00 +0100

    config_pdo.php

--- config_pdo.php
+++ config_pdo.php
@@ -2,7 +2,7 @@
 class DATA_Class 
 {
 // connessione privata passata output dal metodo get_conn()
-private $connessione;
+private $mysqli;
 // parametri per la connessione al database
 private $servername = "localhost";
 private $username = "root";
@@ -12,27 +12,27 @@ private $dbname = "registrazione";
 // funzione per la connessione a MySQL
 public function __construct()
 {
-$this->connessione = new mysqli($this->servername,$this->username,$this->password,$this->dbname);
-// Check connection
-if ($this->connessione->connect_error) {
-  die('Errore di connessione (' . $this->connessione->connect_errno . ') '. $this->connessione->connect_error);
-} else {
-//  echo 'Connesso. ' . $connessione->host_info . "\n";
-}
+  $mysqli = new mysqli('localhost', 'root', 'password');
+  if ($mysqli->connect_error) {
+    die('Errore di connessione (' . $mysqli->connect_errno . ') . $mysqli->connect_error);
+  }
 } // end construct
 
 public function get_conn() {
-return $this->connessione;
+return $this->mysqli;
 }
 } // end class
 
 
 // istanzio classe Iscrizioni che a sua volta istanzia classe DATA_Class 
   $data = new DATA_Class();
-  // dalla DATA_Class mi faccio restituire l'oggetto connessione 
-  $connessione = $data->get_conn();
-  var_dump($connessione);
-
+// dalla DATA_Class mi faccio restituire l'oggetto connessione 
+  $conn = $data->get_conn();
+  //var_dump($conn);
+   // Creo il database
+   $conn->query("CREATE DATABASE biblioteca");
+   // Seleziono il database
+   $conn->query("USE biblioteca");
 
   ?>
   
